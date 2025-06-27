f8cb71b942fbc610ef6d18a242194e3555e2514a
Your Name <riazdev18@gmail.com>
2025-06-27 10:21:06 +0500

    Implemented QuickSort and BubbleSort sorting algorithms

---
+++ Sorting-Algorithms/BubbleSort/Program.cs
@@ -0,0 +1,45 @@
+﻿
+namespace BubbleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] randomNo = new int[100];
+            Random random = new Random();
+            for (int i = 0; i < randomNo.Length; i++)
+            {
+                randomNo[i] = random.Next(60, 101);
+            }
+            Console.WriteLine("Unsorted Array:");
+            PrintArray(randomNo);
+            BubbleSort(randomNo);
+            Console.WriteLine("\nSorted Array");
+            PrintArray(randomNo);
+        }
+       static void PrintArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++) {
+                Console.Write(arr[i]+" ");
+            }
+            Console.ReadLine();
+        }
+
+        static void BubbleSort(int[] arr)
+        { 
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    { 
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Sorting-Algorithms/QuickSort/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace QuickSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] randArray = new int[20];
+            Random rand = new Random();
+            for (int i = 0; i < randArray.Length; i++)
+            {
+                randArray[i] = rand.Next(0, 20);
+            }
+            Console.WriteLine("unSorted Random Array!");
+            PrintArray(randArray);
+            Console.WriteLine("Sorted Array(QuickSort)!");
+            PrintArray(QuickSort(randArray));
+
+
+        }
+        static int[] QuickSort(int[] arr)
+           {
+            if (arr.Length <= 1)
+                return arr;
+
+            Random random = new Random();
+            int pivotIndex = random.Next(0, arr.Length);
+            int pivot = arr[pivotIndex];
+
+            List<int> Left = new List<int>();
+            List<int> Right = new List<int>();
+            List<int> Pivots = new List<int>();
+
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                if (arr[i] < pivot)
+                    Left.Add(arr[i]);
+                else if (arr[i] > pivot)
+                    Right.Add(arr[i]);
+                else
+                    Pivots.Add(arr[i]);
+            }
+
+            var sortedLeft = QuickSort(Left.ToArray());
+            var sortedRight = QuickSort(Right.ToArray());
+
+            return sortedLeft
+                 .Concat(Pivots.ToArray())
+                 .Concat(sortedRight)
+                 .ToArray();
+        }
+       static void PrintArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.ReadLine();
+        }  
+    }
+}
