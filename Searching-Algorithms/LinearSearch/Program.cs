b40575d575272390478cf3af61e3d418f25c748c
Your Name <riazdev18@gmail.com>
2025-06-29 12:49:03 +0500

    Add implementation for LinearSearch

---
+++ Searching-Algorithms/LinearSearch/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq.Expressions;
+
+namespace LinearSearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Random Array Elements:");
+            int[] arr = Utils.GenRandArray();
+            Utils.PrintArray(arr);
+            Console.Write("\nEnter the Element U Wanna Find!:");
+            int Target = int.Parse(Console.ReadLine());
+            List<int> indicesFound =  LinearSearch(arr,Target);
+            if (indicesFound.Count == 0)
+            {
+                Console.WriteLine("Element not Found in the arary");
+            }
+            else
+            {
+                Console.WriteLine("Element Found At Indices:" + string.Join(", ", indicesFound));
+            }
+
+        }
+        static List<int> LinearSearch(int[] arr, int Target)
+        {
+            List<int> indices = new List<int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == Target)
+                {
+                    indices.Add(i);
+                }            
+            }
+            return indices;
+        }
+    }
+}
---
+++ Searching-Algorithms/LinearSearch/Utils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearSearch
+{
+    internal class Utils
+    {
+       public static int[] GenRandArray()
+        {
+            int[] randArray = new int[20];
+            Random random = new Random();
+            for (int i = 0; i < randArray.Length; i++)
+            {
+                randArray[i] = random.Next(0, 20);
+            }
+            return randArray;
+        }
+       public static void PrintArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]+" ");
+            }
+        }
+    }
+}
