ff60d83e9b7f4b5054c7e620c685b8c25ade5111
Your Name <riazdev18@gmail.com>
2025-07-06 12:05:16 +0500

    add implementation for deleteAtlast and delete from specific index

---
+++ ArrayOperations/DeletionFromEnd/Program.cs
@@ -0,0 +1,54 @@
+﻿using CommonUtils;
+
+namespace DeletionFromEnd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] randArr = Utils.GenRandArray();
+            //int[] randArr = {11,22,33,44,55,66,77,88,99,100};
+            int size = randArr.Length;
+            Console.WriteLine("Array Before Deletion:");
+            PrintArray(randArr, size);
+            size = DeletionFromEnd(randArr, size);
+            if (size > 0)
+            {
+                Console.WriteLine("\nArray After Deletion From Last: ");
+                PrintArray(randArr, size);
+            }
+            else
+            {
+                Console.WriteLine("\nArray is empty after deletion.");
+            }
+
+        }
+        static int DeletionFromEnd(int[] randArr,int size)
+        {
+            if (size == 0)
+            {
+                Console.WriteLine("Array is Already empty! can't delete any other Item");
+                return 0;
+            }
+            randArr[size - 1] = 0;
+
+            return size - 1;
+        }
+
+        static void PrintArray(int[] randArr, int size)
+        {
+            if (size == 0)
+            {
+                Console.WriteLine("array has no element to display!");
+                return;
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(randArr[i]+" ");
+            }
+        }
+
+
+    }
+}
---
+++ DeletionFromSpecificIndex/Program.cs
@@ -0,0 +1,40 @@
+﻿using CommonUtils;
+
+namespace DeletionFromSpecificIndex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] randArr = Utils.GenRandArray();
+            //int[] randArr = { 11, 22, 33, 44, 55, 66, 77, 88, 99, 100 };
+            Console.WriteLine("Array Before Deletion:");
+            Utils.PrintArray(randArr);
+            Console.Write("\nEnter the Index of Element You Want To Delete: ");
+            int index = int.Parse(Console.ReadLine()!);
+            if (index < 0 || index >= randArr.Length)
+            {
+                Console.WriteLine("❌ Invalid index! No deletion performed.");
+                return;
+            }
+            Console.WriteLine("\nArray after Deletion:");
+            int[] updatedArr = DeletionFromSpecificIndex(randArr,index);
+            Utils.PrintArray(updatedArr);
+        }
+        static int[] DeletionFromSpecificIndex(int[] randArr, int index)
+        {
+            int[] newArr = new int[randArr.Length - 1];
+
+            for (int i = 0; i<index; i++)
+            {
+                newArr[i] = randArr[i]; 
+            }
+            for (int i = index +1; i < randArr.Length; i++)
+            {
+                newArr[i - 1] = randArr[i];
+            }
+
+            return newArr;
+        }
+    }
+}
